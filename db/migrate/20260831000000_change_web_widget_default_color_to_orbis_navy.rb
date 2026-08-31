75209e4c9fa4899b118334c636b4ba1d84644bc3
Brayan Cardozo <brayancardozo@MacBook-Air-de-Brayan.local>
2026-08-31 16:22:49 -0500

    fix(db): correct the web widget table name in the default color migration

--- db/migrate/20260831000000_change_web_widget_default_color_to_orbis_navy.rb
+++ db/migrate/20260831000000_change_web_widget_default_color_to_orbis_navy.rb
@@ -2,6 +2,6 @@ class ChangeWebWidgetDefaultColorToOrbisNavy < ActiveRecord::Migration[7.1]
   def change
     # New web widgets pick up the Orbis navy launcher instead of the upstream
     # blue. Inboxes created before this migration keep their stored colour.
-    change_column_default :channels_web_widgets, :widget_color, from: '#1f93ff', to: '#010828'
+    change_column_default :channel_web_widgets, :widget_color, from: '#1f93ff', to: '#010828'
   end
 end
